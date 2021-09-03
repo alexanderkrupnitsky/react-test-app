c121a02a5369951cb0ff5bd6be069420f224ab28
alexanderkrupnitsky <AlexanderKrupnitsky@gmail.com>
2021-09-03 22:36:06 +0300

    github 2
    Signed-off-by: alexanderkrupnitsky <AlexanderKrupnitsky@gmail.com>

--- src/layouts/content.jsx
+++ src/layouts/content.jsx
@@ -1,7 +1,7 @@
 const Content = () => {
   return (
     <section className='content'>
-      <p>это Контент в реакте. Это Тест-версия 2</p>
+      <p>это Контент в реакте. Это Тест-версия 3</p>
     </section>
   )
 }
